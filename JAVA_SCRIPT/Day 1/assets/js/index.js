919608cb88b4bb28c960da7785202651a5fb0e42
maya <krish80207@gmail.com>
2026-09-23 11:20:05 +0530

    update

---
+++ JAVA_SCRIPT/Day 1/assets/js/index.js
@@ -0,0 +1,14 @@
+console.log("maya");
+console.log(20);
+console.log("web developer");
+console.log("SLA STUDEND");
+console.log("The age is ");
+alert("incorrect")
+
+
+
+
+
+console.error("error");
+console.warn("waring");
+
