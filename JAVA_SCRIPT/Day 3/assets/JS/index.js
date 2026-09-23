10675858916fb643396daa63fd6546945d05121b
maya <krish80207@gmail.com>
2026-09-23 23:31:29 +0530

    upadte

---
+++ JAVA_SCRIPT/Day 3/assets/JS/index.js
@@ -0,0 +1,132 @@
+let name = "Maya";
+console.log(name);
+let city = "Chennai";
+console.log(city);
+let country = "India";
+console.log(country);
+let course = "JavaScript";
+console.log(course);
+let language = "English";
+console.log(language);
+let subject = "Web Development";
+console.log(subject);
+let college = "ABC College";
+console.log(college);
+let department = "Computer Science";
+console.log(department);
+let gender = "Not Specified";
+console.log(gender);
+let age = "21";
+console.log(age);
+let phone = "9876543210";
+console.log(phone);
+let email = "maya@gmail.com";
+console.log(email);
+let address = "Chennai";
+console.log(address);
+let color = "Blue";
+console.log(color);
+let fruit = "Apple";
+console.log(fruit);
+let animal = "Cat";
+console.log(animal);
+let hobby = "Drawing";
+console.log(hobby);
+let dream = "Software Developer";
+console.log(dream);
+
+// Reassining
+
+// Assignment
+
+name = "krishna";
+console.log(name);
+
+city += "madhurai";
+console.log(city);
+
+country -= "usa";
+console.log(country);
+
+course *= "java"
+console.log(course);
+
+language /= "tamil"
+console.log(language);
+
+
+// Arithmetic
+
+let total ="subject + college";
+console.log(total);
+
+let totall ="department-gender"
+console.log(totall);
+
+let totalll ="department*gender"
+console.log(totalll);
+
+
+
+
+// Numbers
+
+let studentAge = 21;
+let examMark = 85;
+let monthlySalary = 25000;
+let productPrice = 500;
+let productQuantity = 3;
+let totalAmount = 1500;
+let birthYear = 2005;
+let currentYear = 2026;
+let currentMonth = 9;
+let currentDay = 23;
+let testScore = 95;
+let accountBalance = 10000;
+let attendancePercentage = 90;
+
+console.log(studentAge);
+console.log(examMark);
+console.log(monthlySalary);
+console.log(productPrice);
+console.log(productQuantity);
+console.log(totalAmount);
+console.log(birthYear);
+console.log(currentYear);
+console.log(currentMonth);
+console.log(currentDay);
+console.log(testScore);
+console.log(accountBalance);
+console.log(attendancePercentage);
+
+
+let agemark =(studentAge+examMark)
+console.log(agemark);
+
+
+let ages =(studentAge-examMark)
+console.log(ages);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
